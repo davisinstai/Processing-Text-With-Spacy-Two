ef0f95a0c4c5276b36e51064f45b356840485623
davisinstai <117214672+davisinstai@users.noreply.github.com>
2023-09-26 22:12:22 +0000

    commit project 4

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # Processing-Text-With-Spacy-Two
+
+# Project 2c
+
+## Name
+
+## Class year
+
+## Extension(s) - Describe your extension(s) here
+
+## Resources - Who or what did you use to finish this project deliverable?
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
+*Please do not edit below this line!*
+-----------------------------------------------------------------------------------------------------------------------------------------------
+
+## Project Description
+
+The goals of this assignment are:
+* To review basic NLP tasks for words using dictionaries, sets, lists and file read/write.
+
+Here are the steps you should do to successfully complete this project:
+1. From moodle, accept the assignment.
+2. Open and set up a code space (install a python kernal and select it).
+3. Complete the notebook and commit it to Github. Make sure to answer all questions, and to commit the notebook in a "run" state!
+4. Edit the README.md file. Provide your name, your class year, links to/descriptions of any extensions and a list of resources. Make sure to paste the output from running spacy-on-files.py!
+5. Commit your code often. We will take the last commit before the deadline as your submission of the project.
+
+For extra credit:
+* Read the spaCy docs (https://spacy.io/usage/models). Figure out how to make spaCy work for another language. Add a starter question asking the user to indicate the language. 
+* We are doing all of this on the command line. If you got mercury web apps running, make some of this work in mercury.
+* Take a look at the [spaCy universe pdf reader](https://spacy.io/universe/project/spacypdfreader/). Download the pdfs for at least two of the papers in our corpus and load them using this reader. Does this reader give cleaner text then constellate, or not? Justify your answer.
+* Your other ideas are welcome! If you'd like to discuss one with Dr Stent, feel free.
+
+
+## Project Rubric
+
+- [] Notebook is code-complete. (2 points)
+- [] All ten questions in notebook are completely and correctly answered. (10 points)
+- [] File spacy-on-files.py is complete, runs and is commented. (10 points)
+- [] Readme has student's name, class year and resources student used. (3 points)
+- [] Extension (1-2 points for a start; 3-4 points for a complete extension; 5 points for a surprising and creative extension)
+
+### Comments from grader
---
+++ spacy-on-files.py
@@ -0,0 +1,134 @@
+# import glob in case user enters a file pattern
+
+# import zipfile in case user enters a zipfile
+
+# import spacy for nlp
+
+# import re so we can fix up some things in the texts
+
+
+# make a dictionary called 'corpus'
+
+
+# initialize spacy with model 'en_core_web_sm'
+
+
+# try to do this
+try:
+    # ask the user to input a zip file or pattern
+    pattern = 
+    # if pattern ends with '.zip'
+    if 
+        # make a ZipFile of pattern
+        zipf = zipfile.ZipFile(pattern)
+        # for each file_name in the zip file
+        for file_name in zipf.namelist():
+            # print file_name
+
+            # open file_name as f
+            with zipf.open(file_name) as f:
+                # get all the text
+                text = ''.join([x.decode('utf-8') for x in f.readlines()])
+                # run spacy on the text; put the result in the 'corpus' dictionary using file_name as the key
+
+    else: # pattern doesn't end with '.zip'
+        # get everything matching file_name using glob
+        for file_name in glob.glob(pattern):
+            # print file_name
+
+            # open file_name as f
+            with open(file_name, encoding='utf-8') as f:
+                # get all the text
+                text = ''.join(f.readlines())
+                # run spacy on the text; put the result in the 'corpus' dictionary using file_name as the key
+
+# if it doesn't work, say why
+except Exception as e:
+    print(e)
+
+# keep going til the user quits with Ctrl-C
+while True:
+    # print the corpus keys
+
+    # set the document name to something that doesn't exist
+
+    # until the document given by the user is in `corpus`
+    while document not in corpus:
+        # ask the user for a value for `document`
+
+    # set the goal to something that doesn't exist
+
+    # until the goal is 'doc', 'table' or 'statistics'
+    while goal not in ['doc', 'table', 'statistics']:
+        # ask the user for a value for 'goal' from 'doc', 'table' or 'statistics'; don't forget to escape!
+
+        # if the goal is 'doc'
+        if ...:
+            # define 'text' and set the title to be the document key (file name)
+            text = '# ' + document + '\n\n'
+            # walk over the tokens in the document
+
+                # if the token is a noun, add it to 'text' and make it boldface (HTML: <b> at the start, </b> at the end)
+                if :
+
+                # otherwise, if it's a verb, add it to 'text' and make it italicized (HTML: <i> at the start, </i> at the end)
+                elif :
+
+                # otherwise, just add it to 'text'!
+                else:
+
+                # add any whitespace following the token using attribute whitespace_
+                text = text + token.whitespace_
+            # open an output file, named after the document with _text.md appended
+            with open(document + '_text.md', 'w') as outf:
+                # write 'text'
+
+        # if the goal is 'table'
+        elif ...:
+            # Make the tokens table
+            tokens_table = "| Tokens | Lemmas | Coarse | Fine | Shapes | Morphology |\n| ------ | ------ | ------ | ---- | ------ | ---------- | \n"
+            # walk over the tokens in the document
+
+                # if the token's part of speech is not 'SPACE'
+                if :
+                    # add the the text, lemma, coarse- and fine-grained parts of speech, word shape and morphology for this token to `tokens_table`; don't forget the newline character!
+                    tokens_table  = 
+            # Make the entities table
+            entities_table = "| Text | Type |\n| ---- | ---- |\n"
+            # walk over the entities in the document
+                # add the text and label for this entity to 'entities_table'; don't forget the newline character!
+                entities_table = 
+            # open an output file, named after the document with _table.md appended
+            with ...   outf:
+                # write 'tokens_table'
+
+                # write 'entities_table'
+
+        #if the goal is 'statistics'
+        elif ...:
+            # make a dictionary for the statistics called 'stats'
+
+            # walk over the tokens in the document
+            for ...:
+                # if the token's part of speech is not 'SPACE'
+                if token.pos_ != 'SPACE':
+                    # add the token and its part of speech tag ('pos_') to 'stats' (check if it is in 'stats' first!)
+                    if token.text + token.pos_ not in stats:
+                        
+                    # increment its count
+
+            # walk over the entities in the document
+            for ...:
+                # add the entity and its label ('label_') to 'stat's (check if it is in 'stat's first!)
+                if entity.text + entity.label_ not in stats:
+                    
+                # increment its count
+                
+            # open an output file, named after the document with _stats.md appended
+            with ... outf:
+                # write the header for a table of tokens/entities and counts
+                outf.write('| Token/Entity | Count |\n | ------------ | ----- |\n')
+                # print the key and count for each entry in 'stats'
+                for key in sorted(stats.keys()):
+                    # print the key and the count
+                    
